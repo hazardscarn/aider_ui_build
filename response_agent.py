eb681740aa4cbcb642cbaeea03506d4ff9e86a7b
hazardscarn10 <hazardscarn10@gmail.com>
2024-07-28 15:00:58 -0500

    init commit aider

--- response_agent.py
+++ response_agent.py
@@ -1,9 +1,9 @@
 # response_agent.py
-
+import random
 class ResponseAgent:
     def __init__(self):
         self.chat_history = []
 
     def respond(self, user_input):
         # Your chatbot logic goes here
-        return "Response to user input"
+        return str(random.randint(1, 100))
