05fc7c5bee82a54e82447d78abe7ecead644d8dc
hazardscarn10 <hazardscarn10@gmail.com>
2024-07-28 14:55:17 -0500

    Removed unused ResponseAgent import and added user input to chat history.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-from response import ResponseAgent
 
 app = Flask(__name__)
 response_agent = ResponseAgent()
@@ -9,9 +8,11 @@ def index():
     if request.method == 'POST':
         user_input = request.form['user_input']
         response = response_agent.respond(user_input)
+        chat_history.append({'user_input': user_input, 'response': response})
         return render_template('index.html', user_input=user_input, response=response)
 
-    return render_template('index.html')
+    chat_history = response_agent.chat_history
+    return render_template('index.html', chat_history=chat_history)
 
 if __name__ == '__main__':
     app.run(debug=True)
